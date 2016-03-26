728584546a1a190342514ba50f8d789ca35021c1
Carl AMIN <carl@wificampus-030010.grenet.fr>
2016-03-26 23:10:21 +0100

    Interface Produits - Liste Done

---
+++ app/src/main/java/com/example/carl/stockit/AddActivity.java
@@ -0,0 +1,7 @@
+package com.example.carl.stockit;
+
+/**
+ * Created by carl on 26/03/16.
+ */
+public class AddActivity {
+}
--- app/src/main/java/com/example/carl/stockit/MainActivity.java
+++ app/src/main/java/com/example/carl/stockit/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.carl.stockit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,11 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -27,8 +26,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent addIntent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(addIntent);
             }
         });
 
